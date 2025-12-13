01e752580fc7eee31a37ffa829198bc76e60b0d6
satorish <satorizoom51@gmail.com>
2025-12-13 20:20:27 +0100

    upd first script

--- First-C/Hello-You.c
+++ First-C/Hello-You.c
@@ -3,5 +3,14 @@
 int main(void)
 {
     printf("\nHello, You\n");
+
+    int number = 2;
+
+    short sh = 7193;
+
+    unsigned short unsSh = 35000; // только положительные
+
+    printf("Number = %d\n", number);
+    printf("unsigned short = %d\n", unsSh);
     return 0;
 }
